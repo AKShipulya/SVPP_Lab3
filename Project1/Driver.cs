9bf7f52fea344bb398d61f0474c55d89bf77faf2
akshipulya <akshipulya@gmail.com>
2022-04-30 18:09:45 +0300

    Добавьте файлы проекта.

---
+++ Project1/Driver.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+
+    public enum GENDER
+    {
+        male,
+        female,
+        variant
+    };
+
+    public enum COLOR_EYES
+    {
+        brown,
+        green,
+        blue,
+        gray
+    };
+
+    public class Driver : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private int number;
+        private char clazz;
+        private double hgt;
+        private string name;
+        private string address;
+        private GENDER gender;
+        private COLOR_EYES coloredEyes;
+        private DateTime dob;
+        private DateTime iss;
+        private DateTime exp;
+        private bool donor;
+        private string uriImage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Number
+        {
+            get => number;
+            set
+            { 
+                number = value;
+                OnPropertyChanged("Number");
+            }
+        }
+        public char Clazz 
+        {
+            get => clazz; 
+            set
+            {
+                clazz = value;
+                OnPropertyChanged("Clazz");
+            }
+                
+        }
+        public double Hgt 
+        {
+            get => hgt;
+            set
+            {
+                hgt = value;
+                OnPropertyChanged("Hgt");
+            }
+        }
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Address 
+        {
+            get => address;
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+        public GENDER Gender 
+        {
+            get => gender; 
+            set
+            {
+                gender = value;
+                OnPropertyChanged("Gender");
+            }
+                 
+        }
+        public COLOR_EYES ColoredEyes 
+        {
+            get => coloredEyes; 
+            set
+            {
+                coloredEyes = value;
+                OnPropertyChanged("ColoredEyes");
+            }
+        }
+        public DateTime Dob 
+        {
+            get => dob;
+            set
+            {
+                dob = value;
+                OnPropertyChanged("Dob");
+            }
+        }
+        public DateTime Iss 
+        {
+            get => iss;
+            set
+            {
+                iss = value;
+                OnPropertyChanged("Iss");
+            }
+        }
+        public DateTime Exp 
+        { 
+            get => exp;
+            set
+            {
+                exp = value;
+                OnPropertyChanged("Exp");
+            }
+        }
+        public bool Donor {
+            get => donor;
+            set
+            {
+                donor = value;
+                OnPropertyChanged("Donor");
+            }
+        }
+        public string UriImage 
+        { 
+            get => uriImage;
+            set
+            {
+                uriImage = value;
+                OnPropertyChanged("UriImage");
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch(columnName)
+                {
+                    case "Clazz":
+                       if(Clazz < 'A' || Clazz > 'E')
+                        {
+                            error = "Wrong class!";
+                        }
+                        break;
+                    case "Exp":
+                        if(Exp < DateTime.Now)
+                        {
+                            error = "The license is expired!";
+                        }
+                        break;
+                    case "Dob":
+                        if(Dob < new DateTime(1900, 01, 01))
+                        {
+                            error = "Wrong date of birthday!";
+                        }
+                        break;
+                    case "Iss":
+                        if (Iss > DateTime.Now)
+                        {
+                            error = "Wrong date of ISS!";
+                        }
+                        break;
+                }
+                return error;
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Driver's license: №{Number} Class: {Clazz}, from {Iss.ToShortDateString()} to {Exp.ToShortDateString()}." +
+                $"\n {Name}, {Gender}. DOB: {Dob.ToShortDateString()}." +
+                $"\n Address: {Address}. Height: {String.Format("{0:0.00}", Hgt)}, Eyes: {ColoredEyes}." +
+                $"\n Donor status: {(Donor ? "Donor" : "Not donor")}" +
+                $"\n Image: {UriImage}";
+        }
+    }
+}
---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Driver driver = new Driver();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //foreach(COLOR_EYES color in Enum.GetValues(typeof(COLOR_EYES)))
+            //{
+            //    comboBoxEyes.Items.Add(color);
+            //}
+        }
+
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "JPG Files|*.jpg|PNG Files|*.png|All files|*.*";
+            if(dialog.ShowDialog() == true)
+            {
+                driver.UriImage = dialog.FileName;
+                image.Source = new BitmapImage(new Uri(driver.UriImage));
+            }
+        }
+ 
+        private void ButtonLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            driver.Name = "Daenerys Targaryen";
+            driver.Clazz = 'A';
+            driver.Address = "Dragonstone";
+            driver.Number = 9847282;
+            driver.Hgt = 168;
+            driver.Gender = GENDER.female;
+            radioButtonFemale.IsChecked = true;
+            driver.ColoredEyes = COLOR_EYES.green;
+            comboBoxEyes.SelectedItem = driver.ColoredEyes;
+            driver.Dob = new DateTime(1992, 03, 13);
+            datePickerDOB.SelectedDate = driver.Dob;
+            driver.Iss = new DateTime(2015, 01, 22);
+            datePickerISS.SelectedDate = driver.Iss;
+            driver.Exp = new DateTime(2035, 01, 21);
+            datePickerEXP.SelectedDate = driver.Exp;
+            driver.Donor = false;
+            checkBoxDonor.IsChecked = false;
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = System.IO.Path.Combine(sCurrentDirectory, @"..\..\Images\Daenerys.jpg");
+            string filePath = System.IO.Path.GetFullPath(fullPath);
+            driver.UriImage = filePath;
+            image.Source = new BitmapImage(new Uri(driver.UriImage));
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            //int a;
+            //Int32.TryParse(textBoxNumber.Text, out a);
+            //driver.Number = a;
+            //if (textBoxClass.Text.Length > 0)
+            //{
+            //    driver.Clazz = textBoxClass.Text[0];
+            //}
+            //else
+            //{
+            //    driver.Clazz = '-';
+            //}
+            //driver.Name = textBoxName.Text;
+            //driver.Address = textBoxAddress.Text;
+            //if (datePickerDOB.SelectedDate == null || datePickerDOB.SelectedDate.Value == DateTime.Now)
+            //{
+            //    driver.Dob = DateTime.Now;
+            //}
+            //else
+            //{
+            //    driver.Dob = (DateTime)(datePickerDOB.SelectedDate);
+            //}
+            //if (datePickerISS.SelectedDate == null || datePickerISS.SelectedDate.Value == DateTime.Now)
+            //{
+            //    driver.Iss = DateTime.Now;
+            //}
+            //else
+            //{
+            //    driver.Iss = (DateTime)(datePickerISS.SelectedDate);
+            //}
+            //if (datePickerEXP.SelectedDate == null || datePickerEXP.SelectedDate.Value == DateTime.Now)
+            //{
+            //    driver.Exp = DateTime.Now;
+            //}
+            //else
+            //{
+            //    driver.Exp = (DateTime)(datePickerEXP.SelectedDate);
+            //}
+            //driver.Gender = Driver.GENDER.variant;
+            //if(radioButtonFemale.IsChecked == true)
+            //{
+            //    driver.Gender = Driver.GENDER.female;
+            //}
+            //else if(radioButtonMale.IsChecked == true)
+            //{
+            //    driver.Gender = Driver.GENDER.male;
+            //}
+            //if(comboBoxEyes.SelectedIndex > -1)
+            //{
+            //    driver.ColoredEyes = (Driver.COLOR_EYES)comboBoxEyes.SelectedItem;
+            //}
+            //driver.Hgt = sliderHGT.Value;
+            //driver.Donor = (bool)checkBoxDonor.IsChecked;
+
+            MessageBox.Show(driver.ToString());
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxName.Clear();
+            textBoxAddress.Clear();
+            textBoxNumber.Clear();
+            textBoxClass.Clear();
+            datePickerDOB.SelectedDate = null;
+            datePickerISS.SelectedDate = null;
+            datePickerEXP.SelectedDate = null;
+            radioButtonFemale.IsChecked = false;
+            radioButtonMale.IsChecked = false;
+            radioButtonVariant.IsChecked = false;
+            comboBoxEyes.SelectedIndex = 0;
+            checkBoxDonor.IsChecked = false;
+            sliderHGT.Value = 175;
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = System.IO.Path.Combine(sCurrentDirectory, @"..\..\Images\No-Image.png");
+            string filePath = System.IO.Path.GetFullPath(fullPath);
+            image.Source = new BitmapImage(new Uri(filePath));
+        }
+
+        private void newDriver()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = System.IO.Path.Combine(sCurrentDirectory, @"..\..\Images\Snow_photo.jpg");
+            string filePath = System.IO.Path.GetFullPath(fullPath);
+
+            driver.Name = "Jon Snow";
+            driver.Clazz = 'B';
+            driver.Address = "Winterfell";
+            driver.Number = 654234098;
+            driver.Hgt = 187;
+            driver.Gender = GENDER.male;
+            driver.ColoredEyes = COLOR_EYES.brown;
+            driver.Dob = new DateTime(1989, 02, 22);
+            driver.Iss = new DateTime(2018, 03, 14);
+            driver.Exp = new DateTime(2038, 03, 13);
+            driver.Donor = true;
+            driver.UriImage = filePath;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            newDriver();
+            grid.DataContext = driver;
+        }
+    }
+}
